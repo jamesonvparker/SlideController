5883e6ba1d34a744b91bd9ca7db4e28d753c2d0a
jamesonvparker <jamesonvparker@gmail.com>
2021-12-12 13:37:52 -0800

    Created LandingPage for Listing Repurposing Home page for Listings

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   useNavigate
 } from "react-router-dom";
 import Home from "./Pages/Home";
+import LandingPage from "./Pages/LandingPage";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
 import ErrorPage from "./Pages/ErrorPage";
@@ -70,6 +71,7 @@ export default function App() {
             <div className="column is-paddingless">
               <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/listing/:id" element={<LandingPage />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/profile/:username" element={<Profile />} />
---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import { LoginContext } from "../Contexts/LoginContext";
+import SliderCarousel from "../components/SliderCarousel";
+import { SlideContext } from "../Contexts/SlideContext";
+import ImageHero from "../components/ImageHero";
+
+function LandingPage() {
+  const { showProfile, setUsername, setShowProfile, username } = useContext(
+    LoginContext
+  );
+  const { slides } = useContext(SlideContext);
+
+  return (
+    <section className="section is-paddingless">
+      <h1 className="title">Hi {username}</h1>
+      <h2 className="subtitle">This is a Landing Page</h2>
+      <ImageHero imgSrc={"placeholder"} />
+      <SliderCarousel items={slides} />
+
+      <h1 className="title ">Deal of the day</h1>
+      {/* <SliderCarousel items={slides} />
+      <SliderCarousel items={slides} /> */}
+    </section>
+  );
+}
+
+export default LandingPage;
